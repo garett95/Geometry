734623221e3b3e446a092aa7818baa17c676d58f
garett95 <7934650@gmail.com>
2022-08-08 22:47:47 +0300

    Add circle and triangle

---
+++ Geometry/Geometry/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Geometry
+{
+    public class Circle : IFigure
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius {get; }
+
+        public void ValidateCircle()
+        {
+            if (Radius < 0)
+            {
+                throw new ArgumentException("Radius must be greather than 0.");
+            }
+        }
+
+        public double GetSquare() => Math.PI * Math.Pow(Radius, 2);
+    }
+}
---
+++ Geometry/Geometry/Triangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Geometry
+{
+    class Triangle : IFigure
+    {
+        public Triangle(
+            double a,
+            double b,
+            double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+
+        public double GetSquare()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public bool IsRectangularTriangle()
+        {
+            if (A > B && A > C)
+                return A * A == B * B + C * C;
+            else if (B > A && B > C)
+                return B * B == A * A + C * C;
+            else return C * C == A * A + B * B;
+        }
+
+        public bool IsTriangle()
+        {
+            if(A > 0 && B> 0 && C > 0)
+            {
+                if (A + B <= C || A + C <= B || B + C <= A)
+                    return false;
+                else
+                    return true;
+            }
+            throw new ArgumentException("Side of a triangle must be greater than 0.");
+        }
+    }
+}
