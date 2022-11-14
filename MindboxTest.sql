160bd54b1046b12d0d034f4766d23b9c7583dc36
garett95 <89732548+garett95@users.noreply.github.com>
2022-11-14 18:13:50 +0300

    Update MindboxTest.sql

--- MindboxTest.sql
+++ MindboxTest.sql
@@ -3,6 +3,6 @@
 select products.product_name as product, category.product_category as category 
 from products 
 left join products_category
-on products.products_id = products_category.products_id 
+on products.products_id = products_category.products_id
 left join
  category on products_category.category_id=category.category_id;
